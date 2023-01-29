daa31863b3e4484d63c5b690c686e4f375ce9f1d
JY Fang <junyuan.fang@helsinki.fi>
2023-01-29 11:39:45 +0200

    unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -16,17 +16,49 @@ const Button = ({label, handleClick}) =>{
     </>
   )
 }
-const Display = ({label, value, percent}) =>{
+const StatisticLine = ({text, value, percent}) =>{
   if (percent === true) {
     return(
+      <tr>
+      <td>{text}</td>
+      <td>{value} %</td>
+      </tr>
+    )
+  }
+  return(
+    <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = (props) => {
+  
+  const clicked = props.clicked
+  const good = props.good
+  const bad = props.bad
+  const neutral = props.neutral
+  const average = (good-bad)/(good+neutral+bad)
+  const persentage = (good)/(good+neutral+bad) *100
+  if (!clicked){
+    return (
       <>
-      {label} {value} %<br/>
+      <Header text = {"statistics"} />
+      <StatisticLine text = {"No feedback given"}/>
       </>
     )
   }
   return(
     <>
-    {label} {value}<br/>
+    <table>
+    <StatisticLine text = {"good"} value = {good}/>
+    <StatisticLine text = {"neutral"} value = {neutral}/>
+    <StatisticLine text = {"bad"} value = {bad}/>
+    <StatisticLine text = {"all"} value = {good+neutral+bad}/>
+    <StatisticLine text = {"average"} value = {average}/>
+    <StatisticLine text = {"positive"} value = {persentage} percent = {true}/> 
+    </table>
     </>
   )
 }
@@ -35,22 +67,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [clicked, setClick] = useState(false)
+  const clickGood = ()=>{
+    setClick(true)
+    setGood(good + 1)
+    
+  }
+  const clickNeutral = ()=>{
+    setNeutral(neutral + 1)
+    setClick(true)
+  }
+  const clickBad = ()=>{
+    setBad(bad + 1)
+    setClick(true)
+  }
 
-  const average = (good-bad)/(good+neutral+bad)
-  const persentage = (good)/(good+neutral+bad) *100
   return (
     <div>
       <Header text = {"give feedback"}/>
-      <Button label = {"good"} handleClick={()=>setGood(good + 1)}/>
-      <Button label = {"neutral"} handleClick={()=>setNeutral(neutral + 1)}/>
-      <Button label = {"bad"} handleClick={()=>setBad(bad + 1)}/>
+      <Button label = {"good"} handleClick={clickGood}/>
+      <Button label = {"neutral"} handleClick={clickNeutral}/>
+      <Button label = {"bad"} handleClick={clickBad}/>
       <Header text = {"statistics"} />
-      <Display label = {"good"} value = {good}/>
-      <Display label = {"neutral"} value = {neutral}/>
-      <Display label = {"bad"} value = {bad}/>
-      <Display label = {"all"} value = {good+neutral+bad}/>
-      <Display label = {"average"} value = {average}/>
-      <Display label = {"persentage"} value = {persentage} percent = {true}/> 
+      <Statistics good = {good} neutral = {neutral} bad = {bad} clicked={clicked}/>
     </div>
   )
 }
