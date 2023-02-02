d11a7b43ff1a69225a20c2a029dd685c02ed9aa6
JY Fang <junyuan.fang@helsinki.fi>
2023-02-02 11:05:55 +0200

    step9

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,31 +1,57 @@
-const Header = ({ course }) => <h1>{course}</h1>
+const Header = ({ course }) => {<h1>{course}</h1>}
 
-const Content = ({ parts }) => 
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
-  </>
+const Total = ({ parts }) => {
+    console.log("part here is: ", parts)
+    const total = parts.reduce((accumulator,currentValue) =>{
+        console.log('what is happening', accumulator, currentValue.exercises)
+        return accumulator+currentValue.exercises
+    }, 0)
+
+    return (
+        <>
+        <p>total of {total} exercises</p>
+        </>
+    )
+}
+    
+
+
+const Content = ({ parts }) => {
+    return(
+        <>
+        <Part
+          part={parts[0]} 
+        />
+        <Part
+          part={parts[1]} 
+        />
+        <Part
+          part={parts[2]} 
+        />
+        <Part
+          part={parts[3]} 
+        />       
+      </>
+    )
+}
+
+
 
 const Part = ({ part }) => 
 <p>
   {part.name} {part.exercises}
 </p>
 
-console.log("here1")
+
 const Course = ({course})=>{
-    console.log("here2")
     console.log(course)
+    console.log("part is:", course.parts)
+
     return (
         <>
         <Header course={course.name} />
         <Content parts={course.parts} />
+        <Total parts = {course.parts}/>
         </>
     )
 
