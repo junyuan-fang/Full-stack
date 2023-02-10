e3aeaca578622e2294ec664f97296c29093bbf80
JY Fang <junyuan.fang@helsinki.fi>
2023-02-10 10:07:52 +0200

    2c tutorial

--- part2/GettingDataFromServer/src/App.js
+++ part2/GettingDataFromServer/src/App.js
@@ -1,10 +1,33 @@
-import { useState } from '../../part2c/node_modules/@types/react'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+
+    const eventHandler = response =>{
+      console.log('promise fulfilled')
+      setNewNote(response.data)
+    }
+
+    const promise = axios.get('http://localhost:3002/notes')
+    promise.then(eventHandler)
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  useEffect(hook, [])//second parameter of useEffect is used to specify how often the effect is run.
+
+  console.log(notes)
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
